ed176e5077e7e543d850fb80c8d7d32231a3392f
leonardoignacio <leonardoignacio@hotmail.com>
2025-07-23 17:15:09 -0300

    ajuste em allowed_host

--- canesgril/settings.py
+++ canesgril/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DJANGO_DEBUG', '.railway.app', 'False') == 'True'
 #DEBUG=True
 #ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
